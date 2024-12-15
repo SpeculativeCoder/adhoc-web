f643f32cccef0749401999716a7930254493554a
SpeculativeCoder <116658408+SpeculativeCoder@users.noreply.github.com>
2024-12-15 16:52:50 +0000

    Enable default profiles as needed
    For example, we can run the manager application with a different database profile without having to specify all the other needed profiles.

--- adhoc-kiosk/src/main/java/adhoc/AdhocKioskApplication.java
+++ adhoc-kiosk/src/main/java/adhoc/AdhocKioskApplication.java
@@ -23,9 +23,14 @@
 package adhoc;
 
 import adhoc.user.UserRole;
+import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
+
+import java.util.List;
 
 /**
  * When running as a kiosk this application is for access by users (i.e. the "public" facing variant of the application).
@@ -40,6 +45,16 @@ import org.springframework.boot.SpringApplication;
 public class AdhocKioskApplication extends AbstractAdhocApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdhocKioskApplication.class, args); //.start();
+        // rather than rely on spring.profiles.default we will just pick some extra default profiles as needed
+        ConfigurableEnvironment environment = new StandardEnvironment();
+        List<String> activeProfiles = Lists.newArrayList(environment.getActiveProfiles());
+        if (activeProfiles.stream().noneMatch(profile -> profile.startsWith("db-"))) {
+            activeProfiles.add("db-h2postgres");
+        }
+        environment.setActiveProfiles(activeProfiles.toArray(new String[activeProfiles.size()]));
+
+        SpringApplication application = new SpringApplication(AdhocKioskApplication.class);
+        application.setEnvironment(environment);
+        application.run(args);
     }
 }
--- adhoc-manager/src/main/java/adhoc/AdhocManagerApplication.java
+++ adhoc-manager/src/main/java/adhoc/AdhocManagerApplication.java
@@ -24,9 +24,14 @@ package adhoc;
 
 import adhoc.system.artemis.AdhocArtemisConfiguration;
 import adhoc.user.UserRole;
+import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
+
+import java.util.List;
 
 /**
  * When running as a manager, this application talks to a {@link adhoc.hosting.HostingService}
@@ -54,6 +59,22 @@ public class AdhocManagerApplication extends AbstractAdhocApplication {
      * </ul>
      */
     public static void main(String[] args) {
-        SpringApplication.run(AdhocManagerApplication.class, args); //.start();
+        // rather than rely on spring.profiles.default we will just pick default profiles as needed
+        ConfigurableEnvironment environment = new StandardEnvironment();
+        List<String> activeProfiles = Lists.newArrayList(environment.getActiveProfiles());
+        if (activeProfiles.stream().noneMatch(profile -> profile.startsWith("db-"))) {
+            activeProfiles.add("db-h2postgres");
+        }
+        if (activeProfiles.stream().noneMatch(profile -> profile.startsWith("hosting-"))) {
+            activeProfiles.add("hosting-local");
+        }
+        if (activeProfiles.stream().noneMatch(profile -> profile.startsWith("dns-"))) {
+            activeProfiles.add("dns-local");
+        }
+        environment.setActiveProfiles(activeProfiles.toArray(new String[activeProfiles.size()]));
+
+        SpringApplication application = new SpringApplication(AdhocManagerApplication.class);
+        application.setEnvironment(environment);
+        application.run(args);
     }
 }
